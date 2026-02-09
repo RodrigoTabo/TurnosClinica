2536ca888e2822781ad87330b6a35fe88f0a3e50
Rodrigo Ezequiel Tabó <rodrigo_tabo07@hotmail.com>
2026-02-09 10:26:00 -0300

    feat(auth): implement identity core login/register/logout + fix authorizeview

--- Clinica/Program.cs
+++ Clinica/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using TurnosClinica.ApiClients;
 using TurnosClinica.Application.Services;
@@ -10,82 +11,84 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<TurnosDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("TurnosDbContext")));
+builder.Services.AddDbContext<TurnosDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TurnosDbContext")));
+
 builder.Services.AddScoped<ITurnosService, TurnosService>();
 
 builder.Services.AddScoped(sp =>
 {
     var nav = sp.GetRequiredService<NavigationManager>();
-    return new HttpClient { BaseAddress = new Uri(nav.BaseUri) }; // <-- BaseUri, NO Uri
-});
-
-//inyectamos el servicio de Identity.
-builder.Services
-    .AddDefaultIdentity<IdentityUser>(options =>
-    {
-
-
-        //password
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequiredLength = 4;
-        options.Password.RequireLowercase = false;
-        options.Password.RequireUppercase = false;
-
-        // SignIn
-        options.SignIn.RequireConfirmedAccount = false;
-        options.SignIn.RequireConfirmedEmail = false;
-        options.SignIn.RequireConfirmedPhoneNumber = false;
-
-        //User
-        options.User.RequireUniqueEmail = true;
-
-
-    })
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<TurnosDbContext>();
-
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(120);
-    options.SlidingExpiration = true;
-    options.LoginPath = "/Identity/Account/Login";
-    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    return new HttpClient { BaseAddress = new Uri(nav.BaseUri) };
 });
 
 builder.Services.AddScoped<TurnosApiClient>();
 
+builder.Services
+    .AddIdentityCore<IdentityUser>(options =>
+    {
+        // Password
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequiredLength = 4;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireDigit = false;
 
-// Add services to the container.
+        options.SignIn.RequireConfirmedAccount = false;
+        options.User.RequireUniqueEmail = true;
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<TurnosDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignInScheme = IdentityConstants.ApplicationScheme;
+})
+.AddIdentityCookies();
+
+builder.Services.AddAuthorization();
+
+builder.Services.Configure<CookieAuthenticationOptions>(IdentityConstants.ApplicationScheme, options =>
+{
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(120);
+    options.SlidingExpiration = true;
+    options.LoginPath = "/login";
+    options.AccessDeniedPath = "/access-denied";
+});
+
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddCascadingAuthenticationState();
+
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-//Agregamos el servicio de razor pages.
-builder.Services.AddRazorPages();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
 
-app.MapControllers();
-
+// MUST be before mapping endpoints
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapControllers();
+
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+   .AddInteractiveServerRenderMode();
 
-//Mapeamos razor pages
-app.MapRazorPages();
 app.Run();
